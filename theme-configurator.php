b8f45d63ec6a80d404b390d84b6cf370691de314
Igor Prochazka <igor.prochazka@gmail.com>
2011-05-10 18:25:11 +0200

    Initial commit (empty plugin descriptor)

---
+++ theme-configurator.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Theme Configurator
+Plugin URI: http://www.l90r.com/posts/wordpress-theme-configurator
+Description: The most elegant way to add a settings screen to your theme
+Version: 0.1
+Author: Igor Prochazka
+Author URI: http://www.l90r.com/
+
+---------------------------------------------------------------------
+    This file is part of the wordpress plugin "Theme Configurator"
+    Copyright (C) 2009 by Igor Prochazka
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+---------------------------------------------------------------------
+*/
+
+/* register hooks */
+
+
+?>
