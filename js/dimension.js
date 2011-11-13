59b4b99028af448e0634efbdf6310ec2c3cbf4f3
Igor Prochazka <igor.prochazka@gmail.com>
2011-11-13 12:33:17 +0100

    Fix dimension page layout

--- js/dimension.js
+++ js/dimension.js
@@ -1,13 +1,12 @@
 
 (function($) {
     $(function() {
-        $('.thcfg-slider').each(function(idx) {
+        $('.thcfg_slider').each(function(idx) {
             var obj = $(this);
             var min = parseInt(obj.attr('thcfg_min'));
             var max = parseInt(obj.attr('thcfg_max'));
-            var knob = $('<div>');
-            obj.after(knob);
-            knob.slider({
+            var knob = $('<div class="thcfg_knob">');
+            obj.closest('li').find('.thcfg_knob').slider({
                 'min': min,
                 'max': max,
                 'slide': function( event, ui ) {
--- tpl/dimension.php
+++ tpl/dimension.php
@@ -3,13 +3,16 @@
 <?php foreach($items as $item) : $id = $item->id; $title = htmlspecialchars($item->title); ?>
 <?php $value = $values[$id] ?>
         <li>
-            <label for="thcfg_<?php echo $id ?>"><?php echo $title ?></label>
-            <input class="thcfg-slider"
-                   type="text" id="thcfg_<?php echo $id ?>"
-                   name="<?php echo $id ?>"
-                   thcfg_min="<?php echo $item->min ?>"
-                   thcfg_max="<?php echo $item->max ?>"
-                   value="<?php echo $value ?>">
+            <div class="thcfg_left">
+                <label for="thcfg_<?php echo $id ?>"><?php echo $title ?></label>
+                <input class="thcfg_slider"
+                    type="text" id="thcfg_<?php echo $id ?>"
+                    name="<?php echo $id ?>"
+                    thcfg_min="<?php echo $item->min ?>"
+                    thcfg_max="<?php echo $item->max ?>"
+                    value="<?php echo $value ?>">
+            </div>
+            <div class="thcfg_right"><div class="thcfg_knob"></div></div>
         </li>
 <?php endforeach ?>
     </ul>
