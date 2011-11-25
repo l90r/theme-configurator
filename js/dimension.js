5d5fcf76f9cb8d96b9307fadacd46829c01e56a2
Igor Prochazka <igor.prochazka@gmail.com>
2011-11-25 20:41:29 +0100

    Bind sliders to number input

--- js/dimension.js
+++ js/dimension.js
@@ -5,13 +5,17 @@
             var obj = $(this);
             var min = parseInt(obj.attr('thcfg_min'));
             var max = parseInt(obj.attr('thcfg_max'));
-            var knob = $('<div class="thcfg_knob">');
-            obj.closest('li').find('.thcfg_knob').slider({
+			var knob = obj.closest('li').find('.thcfg_knob');
+            knob.slider({
                 'min': min,
                 'max': max,
                 'slide': function( event, ui ) {
     				obj.val(ui.value);
                 }
+			});
+			knob.slider("value", obj.val());
+			obj.change(function() {
+				knob.slider("value", obj.val());
 			});
         });
     });
